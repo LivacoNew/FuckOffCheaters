0efdbe51ac6d19298aa63d3a33b31e97df8477d0
Sam <livaco@livaco.dev>
2024-12-26 09:34:17 +0000

    add tons of mods, removing comments inbetween them since they're mostly redundant

--- lua/tests/mods.lua
+++ lua/tests/mods.lua
@@ -1,65 +1,50 @@
 -- All of these should be lower-case!
 FOffCheaters.DangerousModIDs = {
-	-- Carry Stacker
 	["carrystackerreloaded"] = true,
 	["carry stacker live and reloaded"] = true,
 	["carry stacker 2.0"] = true,
 	["carry stacker"] = true,
-	-- Infinite Ammo
 	["infiniteammo"] = true,
-	-- DLC Unlocker: https://github.com/pd2-stuff/DLC-Unlocker-PD2/tree/master
 	["unlock_only_heists"] = true,
 	["unlock_only_characters"] = true,
 	["unlock_all_dlcs"] = true,
-	-- Cook Faster: https://github.com/DrNewbie/CookFaster
 	["cookfaster"] = true,
-	-- https://modworkshop.net/mod/24425
 	["print faster"] = true,
-	-- Unlimited Pagers: https://modworkshop.net/mod/41337
 	["unlimited pagers"] = true,
-	-- https://modworkshop.net/mod/24657
 	["no pager on domination"] = true,
-	-- https://www.nexusmods.com/payday2/mods/167
 	["cc and money generator"] = true,
-	-- https://modworkshop.net/mod/19579
-	-- i understand it's technically not by default, but can be used as one 
 	["berserker live matters"] = true,
-	-- LockSmasher
 	["locksmasher"] = true,
-	-- Misc 
+	["rngmodifier"] = true,
 	["inf pager"] = true,
 	["infpagers"] = true,
+	["lowcostskill"] = true,
+	["xp increaser (on mission end) - gab"] = true,
+	["old spawnpoint delays"] = true,
 }
 FOffCheaters.DangerousModNames = {
-	-- Carry Stacker
 	["carry stacker reloaded"] = true,
 	["carry stacker: live and reloaded"] = true,
 	["carry stacker2.0"] = true,
 	["carry stacker 2.0"] = true,
-	-- Infinite Ammo
 	["infinite ammo"] = true,
-	-- DLC Unlocker: https://github.com/pd2-stuff/DLC-Unlocker-PD2/tree/master
 	["selective downloadable content"] = true,
 	["character"] = true,
 	["downloadable content"] = true,
-	-- Cook Faster: https://github.com/DrNewbie/CookFaster
 	["cook faster"] = true,
-	-- https://modworkshop.net/mod/24425
 	["print faster"] = true,
-	-- Unlimited Pagers: https://modworkshop.net/mod/41337
 	["unlimited pagers"] = true,
-	-- https://modworkshop.net/mod/24657
 	["no pager on domination"] = true,
-	-- https://www.nexusmods.com/payday2/mods/167
 	["cc and money generator"] = true,
-	-- https://modworkshop.net/mod/19579
-	-- i understand it's technically not by default, but can be used as one 
 	["berserker live matters"] = true,
-	-- LockSmasher
 	["lock smasher"] = true,
-	-- Misc 
+	["rngmodifier"] = true,
 	["inf pagers"] = true,
 	["inf pagers & answer time"] = true,
+	["lowcostskill"] = true,
+	["xp increaser"] = true,
+	["xp increaser (on mission end) - gab"] = true,
+	["old spawnpoint delays"] = true,
 }
 
 function FOffCheaters:CheckMods(peer)
