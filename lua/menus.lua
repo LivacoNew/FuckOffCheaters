6738dd9fdb608f9637f8cfc8211920ca0e061600
Sam <livaco@livaco.dev>
2024-12-26 15:16:47 +0000

    cleanup menus.lua

--- lua/menus.lua
+++ lua/menus.lua
@@ -1,12 +1,12 @@
 Hooks:Add("MenuManagerSetupCustomMenus", "foffcheaters_settings_menu", function()
-	MenuCallbackHandler.FOffCheaters_SaveSettings = function(self)
+	MenuCallbackHandler.FOffCheaters_SaveSettings = function(_)
 		FOffCheaters:SaveSettings()
 	end
-	MenuCallbackHandler.FOffCheaters_SilentModeSetting = function(self, item)
+	MenuCallbackHandler.FOffCheaters_SilentModeSetting = function(_, item)
 		-- just... why blt
 		FOffCheaters.Settings.SilentMode = item:value() == "on"
 	end
-	MenuCallbackHandler.FOffCheaters_LogDetectionsSetting = function(self, item)
+	MenuCallbackHandler.FOffCheaters_LogDetectionsSetting = function(_, item)
 		FOffCheaters.Settings.LogDetections = item:value() == "on"
 	end
 
