081568f8a12954a7df8c668bab929a9e11cdd14f
Sam <livaco@livaco.dev>
2024-12-26 15:16:22 +0000

    add option to toggle logging detections

--- lua/menus.lua
+++ lua/menus.lua
@@ -6,6 +6,9 @@ Hooks:Add("MenuManagerSetupCustomMenus", "foffcheaters_settings_menu", function(
 		-- just... why blt
 		FOffCheaters.Settings.SilentMode = item:value() == "on"
 	end
+	MenuCallbackHandler.FOffCheaters_LogDetectionsSetting = function(self, item)
+		FOffCheaters.Settings.LogDetections = item:value() == "on"
+	end
 
 	MenuHelper:LoadFromJsonFile(FOffCheaters.PathToMod .. "menus/settings.json", FOffCheaters, FOffCheaters.Settings)
 end)
