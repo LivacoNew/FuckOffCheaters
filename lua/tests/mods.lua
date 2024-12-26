95f63dcc2ae452573fdbdaaa35e74dae9ca8f28d
Sam <livaco@livaco.dev>
2024-12-26 09:38:38 +0000

    refactor how mods are stored, merging the two tables and categorising them

--- lua/tests/mods.lua
+++ lua/tests/mods.lua
@@ -1,50 +1,46 @@
 -- All of these should be lower-case!
-FOffCheaters.DangerousModIDs = {
+-- Can be both IDs and Names
+FOffCheaters.DangerousMods = {
+	-- "Can be used normally but still needs to be counted"
+	["berserker live matters"] = true,
+	["freeflightcamera"] = true,
+
+	-- "Depends on who you ask"
 	["carrystackerreloaded"] = true,
+	["carry stacker reloaded"] = true,
 	["carry stacker live and reloaded"] = true,
+	["carry stacker2.0"] = true,
 	["carry stacker 2.0"] = true,
 	["carry stacker"] = true,
-	["infiniteammo"] = true,
+	["lock smasher"] = true,
+	["locksmasher"] = true,
+	-- DLC Unlocker
 	["unlock_only_heists"] = true,
 	["unlock_only_characters"] = true,
 	["unlock_all_dlcs"] = true,
-	["cookfaster"] = true,
-	["print faster"] = true,
-	["unlimited pagers"] = true,
-	["no pager on domination"] = true,
-	["cc and money generator"] = true,
-	["berserker live matters"] = true,
-	["locksmasher"] = true,
-	["rngmodifier"] = true,
-	["inf pager"] = true,
-	["infpagers"] = true,
-	["lowcostskill"] = true,
-	["xp increaser (on mission end) - gab"] = true,
-	["old spawnpoint delays"] = true,
-}
-FOffCheaters.DangerousModNames = {
-	["carry stacker reloaded"] = true,
-	["carry stacker: live and reloaded"] = true,
-	["carry stacker2.0"] = true,
-	["carry stacker 2.0"] = true,
-	["infinite ammo"] = true,
 	["selective downloadable content"] = true,
+	["old spawnpoint delays"] = true,
 	["character"] = true,
 	["downloadable content"] = true,
+
+	-- "Definitely Cheating"
+	["rngmodifier"] = true,
+	["infinite ammo"] = true,
+	["infiniteammo"] = true,
+	["cookfaster"] = true,
 	["cook faster"] = true,
+	["printfaster"] = true,
 	["print faster"] = true,
 	["unlimited pagers"] = true,
 	["no pager on domination"] = true,
 	["cc and money generator"] = true,
-	["berserker live matters"] = true,
-	["lock smasher"] = true,
-	["rngmodifier"] = true,
+	["inf pager"] = true,
+	["infpagers"] = true,
 	["inf pagers"] = true,
 	["inf pagers & answer time"] = true,
 	["lowcostskill"] = true,
 	["xp increaser"] = true,
 	["xp increaser (on mission end) - gab"] = true,
-	["old spawnpoint delays"] = true,
 }
 
 function FOffCheaters:CheckMods(peer)
@@ -54,7 +50,7 @@ function FOffCheaters:CheckMods(peer)
 
 	local isCheater = false
 	for _,v in pairs(mods) do
-		if FOffCheaters.DangerousModIDs[string.lower(v.id)] or FOffCheaters.DangerousModNames[string.lower(v.name)] then
+		if FOffCheaters.DangerousMods[string.lower(v.id)] or FOffCheaters.DangerousMods[string.lower(v.name)] then
 			FOffCheaters:RegisterInfraction(peer, FOffCheaters:Infraction(1, "Found malicious mod: " .. v.name .. " (" .. v.id .. ")"))
 			isCheater = true
 		end
