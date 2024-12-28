6599a42ff0ba8491ddef4da4a8e97ce87ab66128
Sam <livaco@livaco.dev>
2024-12-28 00:18:25 +0000

    left in customfov for testing by accident

--- lua/tests/mods.lua
+++ lua/tests/mods.lua
@@ -4,7 +4,6 @@ FOffCheaters.DangerousMods = {
 	-- "Can be used normally but still needs to be counted"
 	["berserker live matters"] = true,
 	["freeflightcamera"] = true,
-	["customfov"] = true,
 
 	-- "Cheaty but depends on who you ask"
 	["carrystackerreloaded"] = true,
